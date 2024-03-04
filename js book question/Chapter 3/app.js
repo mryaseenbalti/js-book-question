20c070b825bd7579d4af7e4c12b021fd9cb1445d
Muhammad Yaseen <156907279+mryaseenbalti@users.noreply.github.com>
2024-03-04 15:58:23 +0500

    Add files via upload

---
+++ js book question/Chapter 3/app.js
@@ -0,0 +1,14 @@
+var age = "I am 18 years old"
+alert (age);
+
+// question2
+var  visitor = "You have visted this site 14 time"
+alert (visitor);
+
+// question3
+var birthYear = "My birth year is 2004 \n Data type of my ddeclared variable is number"
+console.log (birthYear);
+
+// question4
+var onlineStore = " “JohnDoe ordered 5 T-shirt(s) on XYZ Clothing store”."
+console.log (onlineStore);
